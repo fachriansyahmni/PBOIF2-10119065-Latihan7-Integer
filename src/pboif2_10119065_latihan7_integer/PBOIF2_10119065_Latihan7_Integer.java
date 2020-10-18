8dfa52237e5d7e0ae70d7f17caacd63704e3d91a
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-18 14:53:22 +0700

    upload

---
+++ src/pboif2_10119065_latihan7_integer/PBOIF2_10119065_Latihan7_Integer.java
@@ -0,0 +1,30 @@
+package pboif2_10119065_latihan7_integer;
+
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini menampilkan bilangan integer 
+ *
+ */
+public class PBOIF2_10119065_Latihan7_Integer {
+    
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32234;
+        i = 1_000_000;
+        l = 990000000000L;
+        
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+    }
+    
+}
